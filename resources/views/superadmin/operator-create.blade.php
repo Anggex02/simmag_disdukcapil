b27d6e2a9de9cce48bdcee5b9f2f49931ad5a283
Wildan <zzzwylzzz@gmail.com>
2026-08-02 18:20:09 +0700

    upd2

--- resources/views/superadmin/operator-create.blade.php
+++ resources/views/superadmin/operator-create.blade.php
@@ -8,7 +8,7 @@
 
 @section('content')
 
-    <h1 class="text-3xl font-bold text-white mb-6">
+    <h1 class="text-3xl font-bold text-primary mb-6">
         Tambah Operator
     </h1>
 
@@ -19,27 +19,27 @@
         <div class="space-y-4">
 
             <input type="text" name="name" placeholder="Nama Operator"
-                class="w-full rounded-xl p-3 bg-background border border-bordercolor">
+                class="w-full rounded-xl p-3 bg-background border border-bordercolor text-primary">
 
             <input type="email" name="email" placeholder="Email"
-                class="w-full rounded-xl p-3 bg-background border border-bordercolor">
+                class="w-full rounded-xl p-3 bg-background border border-bordercolor text-primary">
 
             <input type="text" name="no_hp" placeholder="No HP"
-                class="w-full rounded-xl p-3 bg-background border border-bordercolor">
+                class="w-full rounded-xl p-3 bg-background border border-bordercolor text-primary">
 
             <input type="text" name="nip" placeholder="NIP" value="{{ old('nip') }}"
-                class="w-full rounded-xl border border-bordercolor bg-background p-3">
+                class="w-full rounded-xl border border-bordercolor bg-background p-3 text-primary">
 
             <input type="text" name="jabatan" placeholder="Jabatan" value="{{ old('jabatan') }}"
-                class="w-full rounded-xl border border-bordercolor bg-background p-3">
+                class="w-full rounded-xl border border-bordercolor bg-background p-3 text-primary">
 
             <input type="password" name="password" placeholder="Password"
-                class="w-full rounded-xl p-3 bg-background border border-bordercolor">
+                class="w-full rounded-xl p-3 bg-background border border-bordercolor text-primary">
 
             <input type="password" name="password_confirmation" placeholder="Konfirmasi Password"
-                class="w-full rounded-xl p-3 bg-background border border-bordercolor">
+                class="w-full rounded-xl p-3 bg-background border border-bordercolor text-primary">
 
-            <button class="bg-cyan-500 px-6 py-3 rounded-xl text-white">
+            <button class="bg-primary px-6 py-3 rounded-xl text-white">
 
                 Simpan
 
